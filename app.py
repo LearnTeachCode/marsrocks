2c071b3528e499083cc26a218aeac43d41ac4c4c
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-06-21 20:38:02 -0700

    set up basic templates

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,  render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello! Welcome to Transit app. More to come..."
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
