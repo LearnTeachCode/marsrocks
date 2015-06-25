0d449819023ba0e8dafc7a5de520f7b38d2df421
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-06-25 00:18:13 -0700

    add templates and routes for static pages

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/classify')
+def classify():
+    return render_template('classify.html')
+
+@app.route('/stats')
+def stats():
+    return render_template('stats.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
