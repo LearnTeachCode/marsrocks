a565805d158859230072a393c49803589f9a3d32
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-07-08 09:08:29 -0700

    add list_routes to manage.py

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ import os
 import unittest
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
+from flask import url_for
 
 from project import app, db
 
@@ -19,6 +20,27 @@ manager = Manager(app)
 
 # run migration from the commandline use 'db'
 manager.add_command('db', MigrateCommand)
+
+# adds to ability to list routes using "$ python manage.py list_routes"
+# http://flask.pocoo.org/snippets/117/
+@manager.command
+def list_routes():
+    import urllib
+    output = []
+    for rule in app.url_map.iter_rules():
+
+        options = {}
+        for arg in rule.arguments:
+            options[arg] = "[{0}]".format(arg)
+
+        methods = ','.join(rule.methods)
+        url = url_for(rule.endpoint, **options)
+        line = urllib.unquote("{:50s} {:20s} {}".format(rule.endpoint, methods, url))
+        output.append(line)
+
+    for line in sorted(output):
+        print line
+
 
 # adds to ability to run tests using "$ python manage.py test"
 @manager.command
