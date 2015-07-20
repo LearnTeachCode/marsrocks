c9feb9fed664be5792b281469a167f8fd8f9f565
Flora Montufar <benmont99@aol.com>
2015-07-19 20:28:59 -0700

    year will be updated with current year

--- project/shared/global_variables.py
+++ project/shared/global_variables.py
@@ -1,4 +1,10 @@
+import datetime
+
 def get_current_year():
     #Flora, do your code here and set your end result to year.
-    year = 2000
-    return dict(current_year=year)
+    # year = {% now "Y" %}
+
+	now = datetime.datetime.now()
+	year = now.year
+
+	return dict(current_year=year)
