7c6c5a938d425624c85799eae890eef6b41acc9f
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-07-26 15:16:22 -0700

    create dynamic features routes

--- project/features/views.py
+++ project/features/views.py
@@ -1,5 +1,8 @@
 from flask import render_template, Blueprint
 
+from project import db
+from project.models import Classification, Feature
+
 # define blueprints
 features_blueprint = Blueprint(
     'features', __name__,
@@ -8,4 +11,6 @@ features_blueprint = Blueprint(
 
 @features_blueprint.route('/features')
 def index():
-    return render_template('features.html')
+    features = db.session.query(Feature).all()
+
+    return render_template('features.html', features=features, feature=features[0])
