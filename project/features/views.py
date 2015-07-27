4619ccdb0deb0d92714ef5ba3a028405ef36901b
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-07-26 17:25:53 -0700

    set up show all photos for one feature.

--- project/features/views.py
+++ project/features/views.py
@@ -14,3 +14,12 @@ def index():
     features = db.session.query(Feature).all()
 
     return render_template('features.html', features=features, feature=features[0])
+
+@features_blueprint.route('/features/<id>')
+def classifications(id):
+    classifies = Classification.query.filter_by(feature_id=id)
+    photos = []
+    for classify in classifies:
+        photos.append(classify.photo)
+
+    return render_template('one_feature.html', photos=photos, photo=photos[0])
