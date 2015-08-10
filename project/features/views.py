9b686bf3c3106345396a8c32801fe1bdb974c4ab
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-08-09 18:44:13 -0700

    make features and features/id pages work

--- project/features/views.py
+++ project/features/views.py
@@ -13,16 +13,19 @@ features_blueprint = Blueprint(
 def index():
     features = db.session.query(Feature).all()
 
-    return render_template('features.html', features=features, feature=features[0])
+    return render_template('features.html', features=features)
 
 @features_blueprint.route('/features/<id>')
 def classifications(id):
     classifies = Classification.query.filter_by(feature_id=id)
     photos = []
+    ids = {}
     for classify in classifies:
-        photos.append(classify.photo)
+        print classify.photo_id
+        if classify.photo_id not in ids:
+            photos.append(classify.photo)
+            ids[classify.photo_id] = classify.photo_id
 
-    # blah = {'name': 'a', 'id': 1}
-    blah = type(photos[0])
+    feature = Feature.query.filter_by(id=id).first()
 
-    return render_template('one_feature.html', photos=photos, photo=blah)
+    return render_template('one_feature.html', photos=photos, feature=feature)
