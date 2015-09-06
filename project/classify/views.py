f902e548dd0934cfbad343ab96123fc935215182
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-09-06 01:12:07 -0700

    create classify_photo method

--- project/classify/views.py
+++ project/classify/views.py
@@ -16,11 +16,38 @@ classify_blueprint = Blueprint(
 def random_image():
     return db.session.query(Photo).order_by(func.random()).first()
 
+def classify_photo(request):
+    # fields: csrf_token, selected features, hidden photo_id.
+    # loop all the fields.
+    for field in request.form:
+        # ignore csrf_token and photo_id fields
+        if field != 'csrf_token' and field != 'photo_id':
+            # find feature in the database
+            feature = Feature.query.filter_by(slug=field).first()
+
+            # create new classification object
+            classification = Classification(
+                user_id = current_user.id,
+                feature_id = feature.id,
+                photo_id = request.form['photo_id']
+            )
+
+            # save to database
+            db.session.add(classification)
+            db.session.commit()
+
+    # redirect after saving data
+    return redirect(url_for('classify.index'))
+
+
+
 @classify_blueprint.route('/classify', methods = ['POST', 'GET'])
 def index():
     # set up form
     error = None
     form = ClassifyForm(request.form)
+    random_img = random_image()
+
 
     if current_user.is_authenticated():
         template = 'classify.html'
@@ -29,34 +56,15 @@ def index():
 
     if request.method == 'POST' and form.validate():
         # request.form contains a dictionary of all the fields in the form
+        classify_photo(request)
 
-        # fields: csrf_token, selected features, hidden photo_id.
-        # loop all the fields.
-        for field in request.form:
-            # ignore csrf_token and photo_id fields
-            if field != 'csrf_token' and field != 'photo_id':
-                # find feature in the database
-                feature = Feature.query.filter_by(slug=field).first()
 
-                # create new classification object
-                classification = Classification(
-                    user_id = current_user.id,
-                    feature_id = feature.id,
-                    photo_id = request.form['photo_id']
-                )
-
-                # save to database
-                db.session.add(classification)
-                db.session.commit()
-
-        # redirect after saving data
-        return redirect(url_for('classify.index'))
 
     # render the template
     return render_template(template,
         current_user = current_user,
         form = form,
         error = error,
-        photo = random_image()
+        photo = random_img
     )
 
